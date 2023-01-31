d0023ec8ac2646dc2ebb344666cf51060af60461
saikiranreddyappidi <saikiranreddyvfstr@gmail.com>
2023-02-01 03:54:08 +0530

    Added some new features and fixed some bugs.New UI has been introduced

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -5,107 +5,118 @@ import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Scanner;
 import java.util.Stack;
 
 class EvaluateString
 {
-    public static int evaluate(String expression)
+    public static double evaluate(String input)
     {
-        char[] tokens = expression.toCharArray();
-        Stack<Integer> values = new
-                Stack<Integer>();
-        Stack<Character> ops = new
-                Stack<Character>();
-
-        for (int i = 0; i < tokens.length; i++)
-        {
-            if (tokens[i] == ' ')
-                continue;
-            if (tokens[i] >= '0' &&
-                    tokens[i] <= '9')
-            {
-                StringBuilder sbuf = new
-                        StringBuilder();
-                while (i < tokens.length &&
-                        tokens[i] >= '0' &&
-                        tokens[i] <= '9')
-                    sbuf.append(tokens[i++]);
-                values.push(Integer.parseInt(sbuf.
-                        toString()));
-                i--;
-            }
-            else if (tokens[i] == '(')
-                ops.push(tokens[i]);
-            else if (tokens[i] == ')')
-            {
-                while (ops.peek() != '(')
-                    values.push(applyOp(ops.pop(),
-                            values.pop(),
-                            values.pop()));
-                ops.pop();
-            }
-            else if (tokens[i] == '+' ||
-                    tokens[i] == '-' ||
-                    tokens[i] == '*' ||
-                    tokens[i] == '/')
-            {
-                while (!ops.empty() &&
-                        hasPrecedence(tokens[i],
-                                ops.peek()))
-                    values.push(applyOp(ops.pop(),
-                            values.pop(),
-                            values.pop()));
-                ops.push(tokens[i]);
-            }
-        }
         try{
-            while (!ops.empty())
-                values.push(applyOp(ops.pop(),
-                        values.pop(),
-                        values.pop()));
-            return values.pop();
+            Stack<Integer> op  = new Stack<Integer>();
+            Stack<Double> val = new Stack<Double>();
+            Stack<Integer> optmp  = new Stack<Integer>();
+            Stack<Double> valtmp = new Stack<Double>();
+            input = "0" + input;
+            input = input.replaceAll("-","+-");
+            String temp = "";
+            for (int i = 0;i < input.length();i++)
+            {
+                char ch = input.charAt(i);
+                if (ch == '-')
+                    temp = "-" + temp;
+                else if (ch != '+' &&  ch != '*' && ch != '/' && ch!='%')
+                    temp = temp + ch;
+                else
+                {
+                    val.push(Double.parseDouble(temp));
+                    op.push((int)ch);
+                    temp = "";
+                }
+            }
+            val.push(Double.parseDouble(temp));
+            char[] operators = {'/','*','%','+'};
+            for (int i = 0; i < 4; i++)
+            {
+                boolean it = false;
+                while (!op.isEmpty())
+                {
+                    int optr = op.pop();
+                    double v1 = val.pop();
+                    double v2 = val.pop();
+                    if (optr == operators[i])
+                    {
+                        if (i == 0)
+                        {
+                            double res=0;
+                            if (v1 == 0)
+                            {
+                                System.out.println("Can't divide by 0");
+                            }
+                            else {
+                                res = v2 / v1;
+                            }
+                            valtmp.push(res);
+                            it = true;
+                            break;
+                        }
+                        else if (i == 1)
+                        {
+                            valtmp.push(v2 * v1);
+                            it = true;
+                            break;
+                        }
+                        else if(i==2) {
+                            double res=0;
+                            if (v1 == 0)
+                            {
+                                System.out.println("Can't divide by 0");
+                            }
+                            else {
+                                res = v2 % v1;
+                            }
+                            valtmp.push(res);
+                            it = true;
+                            break;
+                        }
+                        else {
+                            valtmp.push(v2 + v1);
+                            it = true;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        valtmp.push(v1);
+                        val.push(v2);
+                        optmp.push(optr);
+                    }
+                }
+                while (!valtmp.isEmpty())
+                    val.push(valtmp.pop());
+                while (!optmp.isEmpty())
+                    op.push(optmp.pop());
+                if (it)
+                    i--;
+            }
+            double value=val.pop();
+            System.out.println("\nResult = "+value);
+            return value;
         }
         catch (Exception e){
             System.out.println("Invalid Expression");
-            return 0;
+            return 0.00;
         }
     }
-    public static boolean hasPrecedence(
-            char op1, char op2)
-    {
-        if (op2 == '(' || op2 == ')')
-            return false;
-        return (op1 != '*' && op1 != '/') ||
-                (op2 != '+' && op2 != '-');
-    }
-    public static int applyOp(char op,
-                              int b, int a)
-    {
-        switch (op)
-        {
-            case '+':
-                return a + b;
-            case '-':
-                return a - b;
-            case '*':
-                return a * b;
-            case '/':
-                if (b == 0)
-                    throw new
-                            UnsupportedOperationException(
-                            "Cannot divide by zero");
-                return a / b;
-        }
-        return 0;
-    }
-
 }
+
 
 
 class construct{
     char num;
     String arr="";
-    float result=0;
+    double result=0;
     public String extend(char num){
         this.num = num;
         arr+=num;
@@ -113,13 +124,22 @@ class construct{
         return arr;
     }
     public String ans() {
-        int i=arr.length()-1;
-        if(arr.charAt(i)=='+'||arr.charAt(i)=='-'||arr.charAt(i)=='*'||arr.charAt(i)=='/'){
-            System.out.println("Invalid Expression");
+        try{
+            int i=arr.length()-1;
+            if(arr.charAt(i)=='+'||arr.charAt(i)=='-'||arr.charAt(i)=='*'||arr.charAt(i)=='/'||arr.charAt(i)=='%'||arr.charAt(i)=='.'||
+                    (arr.charAt(i)=='0' &&arr.charAt(i-1)=='/')
+            )
+            {
+                System.out.println("Invalid Expression");
+                return String.valueOf(result);
+            }
+            result = EvaluateString.evaluate(arr);
             return String.valueOf(result);
         }
-        result = EvaluateString.evaluate(arr);
-        return String.valueOf(result);
+        catch (Exception e){
+            System.out.println("Invalid Expression an exception");
+            return String.valueOf(result);
+        }
     }
 }
 public class MainActivity extends AppCompatActivity {
@@ -173,6 +193,16 @@ public class MainActivity extends AppCompatActivity {
         input.setText(str.extend('0'));
         output.setText(str.ans());
     }
+    public void doubleZero(View view) {
+        input.setText(str.extend('0'));
+        input.setText(str.extend('0'));
+        output.setText(str.ans());
+    }
+    public void equal(View view) {
+        str.arr= str.ans();
+        input.setText(str.arr);
+        output.setText("0");
+    }
     public void plus(View view) {
         input.setText(str.extend('+'));
     }
@@ -184,6 +214,19 @@ public class MainActivity extends AppCompatActivity {
     }
     public void divide(View view) {
         input.setText(str.extend('/'));
+    }
+    public void modulus(View view) {
+        input.setText(str.extend('%'));
+    }
+    public void dot(View view) {
+        input.setText(str.extend('.'));
+    }
+    public void backspace(View view) {
+        if(str.arr.length()>0) {
+            str.arr = str.arr.substring(0, str.arr.length() - 1);
+            input.setText(str.arr);
+            output.setText(str.ans());
+        }
     }
     public void clear(View view) {
         input.setText("");
